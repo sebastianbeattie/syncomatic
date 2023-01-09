0d2cf1d61f0ccee64d5813b23a70c43733f3532f
ElementalMP4 <sebahudl@gmail.com>
2023-01-09 14:12:59 +0000

    Add a shorter command name thingy

--- install_client.sh
+++ install_client.sh
@@ -2,3 +2,5 @@ pip3 install -r client/requirements.txt
 
 sudo cp client/syncomatic.py /usr/local/bin/syncomatic
 sudo chmod +x /usr/local/bin/syncomatic
+
+sudo ln -s /usr/local/bin/syncomatic /usr/local/bin/sm
